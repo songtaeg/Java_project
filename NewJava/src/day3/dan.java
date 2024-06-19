3114b409a3aad6f4348a16dd947d4e5cca468b2a
TJ-BU-708-P16 <TJ-BU-708-P16@DESKTOP-BGM4HT6>
2024-06-19 17:37:51 +0900

    이름 변경

--- NewJava/src/day3/dan.java
+++ NewJava/src/day3/dan.java
@@ -3,7 +3,7 @@ package day3;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Test {
+public class dan {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
