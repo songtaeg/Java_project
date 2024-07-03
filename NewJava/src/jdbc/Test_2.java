b54729c907b3e64017b4f71cbc0f70742ad66351
TJ-BU-708-P16 <TJ-BU-708-P16@DESKTOP-BGM4HT6>
2024-07-03 18:11:25 +0900

    수정

--- NewJava/src/jdbc/Test_2.java
+++ NewJava/src/jdbc/Test_2.java
@@ -25,7 +25,7 @@ public class Test_2 {
 			boolean flag=false;
 			
 			for(int i=0; i<arr.length; i++) {
-				if(i!=4 && arr[i]==n1) {
+				if( arr[i]==n1) {
 					System.out.println("다시 입력");
 					flag=true;
 					break;
